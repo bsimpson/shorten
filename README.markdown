f9ca934c00f1850ad845278bea4ad7f89d6b1864
Ben Simpson <ben@mojotech.com>
2012-05-02 00:21:50 -0400

    Add Shorten::Word class
    Encode and decode functionality implemented

--- README.markdown
+++ README.markdown
@@ -8,7 +8,29 @@ If a word can be shortened, it is: `"quintessential" => "quintessent"`
 If a word cannot be shortened, the entire word is used: `"the" => "the"`
 
 # Usage
-    require 'shorten.rb'
-    shortener = Shorten.new
-    shortener.shorten("quintessential") # => "quintessent"
-    shortener.shorten_sentence("The expeditious brown omnivorous mammal leapfrogged over the lethargic canine") # => "the expeditious brown omnivoro mammal leapfrogge over the lethargic canine"
+
+## Require the library
+    require './shorten'
+
+## Instantiate the class, and populate the dictionaries
+    shorten = Shorten.new
+
+## Shorten a single word
+    word = shorten.word("quintessential").shorten # => "quintessent"
+    word.class # => Shorten::Word
+    word.truncated? # => true
+    word.original # => "quintessential"
+
+## Shorten an entire sentence
+    shorten.sentence("The expeditious brown omnivorous mammal leapfrogged over the lethargic canine")
+    # => "the expeditious brown omnivoro mammal leapfrogge over the lethargic canine"
+
+## Encode words/sentences
+Encoding suffixes shortened words with a \t character to indicate it was truncated. If you are just crazy enough, you can truncate and encode sentences as a poor man's compression algorithm.
+
+    shorten.encode("The expeditious brown omnivorous mammal leapfrogged over the lethargic canine")
+    # => "the expeditious brown omnivoro\t mammal leapfrogge\t over the lethargic canine"
+
+## Decode encoded words/sentences
+    shorten.decode("the expeditious brown omnivoro\t mammal leapfrogge\t over the lethargic canine")
+    # => "The expeditious brown omnivorous mammal leapfrogged over the lethargic canine"
--- shorten.rb
+++ shorten.rb
@@ -1,30 +1,83 @@
 class Shorten
   DICT_PATH = '/usr/share/dict/words'
-  DICT_LENGTH = `wc -l #{DICT_PATH}`
 
+  # Create a dict array for each letter of the alphabet
+  # These are populated with DICT_PATH contents
   def initialize
     a_through_z = ('a'..'z').to_a
     a_through_z.each {|x| instance_variable_set(:"@dict_#{x}", [])}
     File.open(DICT_PATH).each do |line|
       line.downcase!
       next unless a_through_z.include?(line[0])
+      next if line[/'s$/]
       instance_variable_get(:"@dict_#{line[0]}").push(line.chomp)
     end
   end
 
-  def shorten(word)
-    word.downcase!
-    (1..word.length).to_a.each do |x|
-      prefix = word.slice(0, x)
-      words_matching_prefix = Proc.new {|y| y[/^#{prefix}/]}
-      matches = instance_variable_get(:"@dict_#{word[0]}").select(&words_matching_prefix)
-      return prefix if matches.length <= 1
-    end
-    word
+  def sentence(arg="the expeditious brown omnivorous mammal leapfrogged over the lethargic canine")
+    word_lookup = Proc.new {|word| Word.new(word, self).shorten }
+    arg.split.map(&word_lookup).join(' ')
   end
 
-  def shorten_sentence(sentence="the expeditious brown omnivorous mammal leapfrogged over the lethargic canine")
-    word_lookup = Proc.new {|x| shorten(x) }
-    sentence.split.map(&word_lookup).join(' ')
+  # Same as #sentence, except encode shortened words by marking them with a \t suffix
+  def encode(arg)
+    word_lookup = Proc.new do |word|
+      output = ""
+      output += _word = Word.new(word, self).shorten
+      output += "\t" if _word.truncated?
+      output
+    end
+    arg.split.map(&word_lookup).join(' ')
+  end
+
+  # Decode an encoded sentence, expanding words that are shortened
+  def decode(arg)
+    word_lookup = Proc.new {|word| Word.new(word, self).expand}
+    # This is a regex so that whitespace (\t) is not ignored
+    arg.split(/ /).map(&word_lookup).join(' ')
+  end
+
+  # Return an instance of Shorten::Word
+  def word(arg)
+    Word.new(arg, self)
+  end
+
+  class Word < String
+    attr_accessor :truncated, :original
+    protected :original=, :truncated=
+    alias :"truncated?" :truncated
+
+    def initialize(arg="quintessential", shorten)
+      @truncated = false
+      @original = arg.downcase
+      # Reference to outer class Shorten
+      @shorten = shorten
+      super(arg)
+    end
+
+    # Take a word, and shorten it to be unambiguous if possible
+    # Shortened words will store the #original, and whether they were #truncated
+    def shorten
+      (1..@original.length).to_a.each do |x|
+        prefix = @original.slice(0, x)
+        words_matching_prefix = Proc.new {|y| y[/^#{prefix}/]}
+        matches = @shorten.instance_variable_get(:"@dict_#{@original[0]}").select(&words_matching_prefix)
+        if matches.length <= 1
+          _word = self.class.new(prefix, @shorten)
+          _word.truncated = true
+          _word.original = @original
+          return _word
+        end
+      end
+      _word = self.class.new(@original, @shorten)
+    end
+
+    # Given a word that was shortened and encoded, expand that word to its original value
+    def expand
+      return self unless @original[/\t$/]
+      word_without_expansion_encoding = @original.gsub(/\t/,'')
+      match = Proc.new {|word| word[/^#{word_without_expansion_encoding}/]}
+      @shorten.instance_variable_get(:"@dict_#{@original[0]}").detect(&match)
+    end
   end
 end
